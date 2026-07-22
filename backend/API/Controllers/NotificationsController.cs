293b3829a0eaa9c8c359286f256888094502910d
Edirisinghe EMCP <emcpedirisinghe@students.nsbm.ac.lk>
2026-07-22 13:01:09 +0530

    Add authorization to NotificationsController
    Added authorization to NotificationsController.

--- backend/API/Controllers/NotificationsController.cs
+++ backend/API/Controllers/NotificationsController.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Services;
 
+using Microsoft.AspNetCore.Authorization;
+
 namespace API.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class NotificationsController : ControllerBase
