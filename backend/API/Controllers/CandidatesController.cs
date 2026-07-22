8df0235b14b2f6687a3a0873e7ec88017a08154f
Edirisinghe EMCP <emcpedirisinghe@students.nsbm.ac.lk>
2026-07-22 12:56:02 +0530

    Add authorization attribute to CandidatesController

--- backend/API/Controllers/CandidatesController.cs
+++ backend/API/Controllers/CandidatesController.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Persistence;
 
+using Microsoft.AspNetCore.Authorization;
+
 namespace API.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class CandidatesController : ControllerBase
